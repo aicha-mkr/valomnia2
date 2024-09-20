3f6916662dcde34d11c62e03c503662f0dd76820
aicha-mkr <118392297+aicha-mkr@users.noreply.github.com>
2024-09-20 23:36:15 +0100

    Initial commit

---
+++ resources/views/layouts/sections/footer/footer.blade.php
@@ -0,0 +1,9 @@
+@php
+$containerFooter = !empty($containerNav) ? $containerNav : 'container-fluid';
+@endphp
+
+<!-- Footer-->
+<footer class="content-footer footer bg-footer-theme">
+ 
+</footer>
+<!--/ Footer-->
